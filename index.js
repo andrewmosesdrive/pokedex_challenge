a23dbafa3919c782eedd756e57b7ed6874b02644
Andrew Moses <andrewmosesdrive@gmail.com>
2020-10-31 20:02:03 -0700

    api connected and data now shows on the html

--- index.js
+++ index.js
@@ -2,16 +2,30 @@ const pokecontainer = document.getElementById("pokecontainer");
 const pokemon_count = 150;
 
 const fetchPokemon = async () => {
-    for(let i = 1; i <= pokemon_count.length; i++) {
-        await getPokemon;
+    for(let i = 1; i <= pokemon_count; i++) {
+        await getPokemon(i);
     }
-}
+};
 
 const getPokemon = async id => {
-    const url = `https:/pokeapi.co/api/v2/pokemon/${id}`;
-    const result = await fetch(url);
-    const pokemon = await result.json();
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const res = await fetch(url);
+    const pokemon = await res.json();
     console.log(pokemon);
+    createPokeEntry(pokemon);
 };
-console.log(1)
-const
+
+fetchPokemon();
+
+function createPokeEntry(pokemon) {
+    const pokemonEl = document.createElement("div");
+    pokemonEl.classList.add("pokemon");
+
+    const pokeInnerHTML = `${pokemon.id}`
+
+    pokemonEl.innerHTML = pokeInnerHTML;
+
+    pokecontainer.appendChild(pokemonEl)
+}
+
+
