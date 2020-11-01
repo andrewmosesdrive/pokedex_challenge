5390d483b64e4e890d65d4abf007383d9d066004
Andrew Moses <andrewmosesdrive@gmail.com>
2020-10-31 23:31:08 -0700

    cards updated to color code with type; some styling completed. need to expand cards on click and include a search feature

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const pokecontainer = document.getElementById("pokecontainer");
 const pokemon_count = 150;
 
+// pokemon color scheme created by @florinpop17
+const colors = {
+	fire: '#FDDFDF',
+	grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
+};
+
+const primary_attributes = Object.keys(colors);
+
 const fetchPokemon = async () => {
     for(let i = 1; i <= pokemon_count; i++) {
         await getPokemon(i);
@@ -11,7 +31,7 @@ const getPokemon = async id => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const res = await fetch(url);
     const pokemon = await res.json();
-    console.log(pokemon);
+    // console.log(pokemon);
     createPokeEntry(pokemon);
 };
 
@@ -21,7 +41,26 @@ function createPokeEntry(pokemon) {
     const pokemonEl = document.createElement("div");
     pokemonEl.classList.add("pokemon");
 
-    const pokeInnerHTML = `${pokemon.id}`
+    const poke_attributes = pokemon.types.map(element => element.type.name);
+
+    const attributeSort = primary_attributes.find(type => poke_attributes.indexOf(type) > -1);
+
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+
+    console.log(attributeSort)
+    const color = colors[attributeSort];
+    pokemonEl.style.backgroundColor = color;
+
+
+    const pokeInnerHTML = `
+          <div class="img-container">
+          <img src="${pokemon.sprites.front_default}"</div> 
+            <div><span>#${pokemon.id}</span>
+            <h5 class="card-title">${name}</h5>   
+            <h6>Type: ${attributeSort}</h6>
+            </div>
+            </div>
+        `
 
     pokemonEl.innerHTML = pokeInnerHTML;
 
